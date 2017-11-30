4d6fcfb9f7b48c85a712f112af3e045e0e519cb9
Jaime Spacco <jspacco@knox.edu>
2017-11-29 19:06:37 -0600

    added new test case to trigger Travis

--- test.py
+++ test.py
@@ -15,6 +15,9 @@ class TestUM(unittest.TestCase):
     def test_numbers_0_1(self):
         self.assertEqual(multiply(0, 1), 0)
 
+    def test_numbers_1_2(self):
+        self.assertEqual(multiply(1, 2), 2)
+
 
 if __name__ == '__main__':
     unittest.main()
